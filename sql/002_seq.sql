40c34669aa0751e95d8e633ff0d52348e243096d
miguelescorpusa-stack <miguelescorpusa@gmail.com>
2025-10-12 15:17:25 -0400

    Update 002_seq.sql

--- sql/002_seq.sql
+++ sql/002_seq.sql
@@ -1,8 +1,7 @@
 -- Secuencia para ref_code: MC-00201 en adelante
 CREATE SEQUENCE IF NOT EXISTS shipment_seq START WITH 201;
 
--- Generador de código MC-00xxx
+-- Generador MC-00xxx
 CREATE OR REPLACE FUNCTION mc_next_ref() RETURNS TEXT AS $$
   SELECT 'MC-' || TO_CHAR(NEXTVAL('shipment_seq'), 'FM00000');
 $$ LANGUAGE SQL;
-
