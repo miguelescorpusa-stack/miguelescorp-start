7ecb4bee34b87c82e253a18acc2a3758c2228b28
miguelescorpusa-stack <miguelescorpusa@gmail.com>
2025-10-12 09:15:30 -0400

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,13 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+const DATABASE_URL = process.env.DATABASE_URL!;
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: DATABASE_URL,
+  ssl:
+    DATABASE_URL.includes("neon.tech") ||
+    DATABASE_URL.includes("supabase.co") ||
+    DATABASE_URL.includes("sslmode=require")
+      ? { rejectUnauthorized: false }
+      : undefined
 });
